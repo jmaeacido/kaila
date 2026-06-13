378e48b68916e65602b68d4a31adb77ae5ae9593
jmaeacido <94jmaea94@gmail.com>
2026-06-13 06:21:37 +0000

    feat(ui-polish): update styles and scripts for consumer UI enhancements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kaila-pwa-v180-mobile-flow-redesign";
+const CACHE_NAME = "kaila-pwa-v182-consumer-ui-polish-2";
 const IS_ANDROID_WEBVIEW = /\bwv\b/i.test(navigator.userAgent || "");
 const IS_NATIVE_WEBVIEW = IS_ANDROID_WEBVIEW
   && (self.location.protocol === "capacitor:"
